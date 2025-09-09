fc342e88aea7f675032629e9d267a2e65292c4cf
Adrian Jasionowicz <jasionowiczadrian@gmail.com>
2025-09-09 21:09:53 +0200

    Prepare to make pdf export, delete unused endpoints

--- src/main/java/jasionowicz/warhammer/builder/LoginUser/SettingsController.java
+++ src/main/java/jasionowicz/warhammer/builder/LoginUser/SettingsController.java
@@ -1,7 +1,11 @@
 package jasionowicz.warhammer.builder.LoginUser;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import jasionowicz.warhammer.builder.LoginUser.model.UpdateEmailRequest;
+import jasionowicz.warhammer.builder.LoginUser.model.UpdatePasswordRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,28 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SettingsController {
 
-    private LoginUserService loginUserService;
+    private final SettingsService settingsService;
 
-    public SettingsController(LoginUserService loginUserService) {
-        this.loginUserService = loginUserService;
+    public SettingsController(SettingsService settingsService) {
+        this.settingsService = settingsService;
     }
 
-    @PostMapping("/user/setEmail/")
-    public ResponseEntity<?> setEmail(@RequestParam  String email,@RequestParam String password, Authentication authentication) {
-        loginUserService.setUserEmail(email,password,authentication);
+    @PostMapping("/user/setEmail")
+    public ResponseEntity<?> setEmail(@RequestBody UpdateEmailRequest request, Authentication authentication) {
 
+        settingsService.setUserEmail(request.getEmail(), request.getPassword(), authentication);
         return ResponseEntity.ok().build();
     }
+
     @PostMapping("/user/setPassword")
-    public ResponseEntity<?> setPassword(@RequestParam String password,@RequestParam String newPassword, Authentication authentication) {
-        loginUserService.setUserPassword(password,newPassword,authentication);
+    public ResponseEntity<?> setPassword(@RequestBody UpdatePasswordRequest request, Authentication authentication) {
+        settingsService.setUserPassword(request.getPassword(), request.getNewPassword(), authentication);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/user/getUserInfo")
-    public ResponseEntity<LoginUserPreview> getUserInfo( Authentication authentication) {
-       LoginUserPreview loginUserPreview = loginUserService.getUserInfo(authentication);
-       return ResponseEntity.ok().body(loginUserPreview);
+    public ResponseEntity<LoginUserPreview> getUserInfo(Authentication authentication) {
+        LoginUserPreview loginUserPreview = settingsService.getUserInfo(authentication);
+        return ResponseEntity.ok().body(loginUserPreview);
     }
-
 }
